5576bb86ba78a45e5039110b08681cfdd3bc5fa4
Elicia <elicia_cor@hotmail.com>
2013-01-25 16:16:44 -0500

    crear perfil

--- sisgedo/principal/models.py
+++ sisgedo/principal/models.py
@@ -3,3 +3,19 @@ from django.contrib.auth.models import User
 
 User.add_to_class('direccion', models.CharField(null=True,blank=True, max_length=500))
 User.add_to_class('telefono', models.PositiveIntegerField(null=True,blank=True))
+
+
+class PerfilUsuario(models.Model):
+	TIPO=(
+			('1','operario'),
+			('2','supervisor'),
+			('3','administrador'),
+		)
+	tipo = models.CharField(max_length=5,choices=TIPO)
+	fecha_registro = models.DateField() 
+	fecha_vigencia = models.DateField() 
+	estado= models.BooleanField()
+	usuario =models.ForeignKey(User)
+
+	def __unicode__(self):
+		return '%s y %s' %(self.usuario,self.tipo)
